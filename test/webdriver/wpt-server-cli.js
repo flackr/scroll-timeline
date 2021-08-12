eb92f4defb1d2808da9f985cc4fffe2687f418ec
Robert Flack <flackr@gmail.com>
2021-08-12 10:53:18 -0400

    Make npm run test:simple listen on localhost interface by default.
    Generally testing will be done locally. The environment variable
    WPT_SERVER_ADDRESS can be used to change this.

--- test/webdriver/wpt-server-cli.js
+++ test/webdriver/wpt-server-cli.js
@@ -3,10 +3,11 @@ const TEST_CONFIGS = require("../tests.config.json");
 const harnessTests = require("./harness-tests");
 
 // Env configs
+WPT_SERVER_ADDRESS = process.env.WPT_SERVER_ADDRESS || "127.0.0.1";
 WPT_SERVER_PORT = process.env.WPT_SERVER_PORT || "8081";
 WPT_DIR = process.env.WPT_DIR || "test/wpt";
 
 (async () => {
     let harnessTestUrls = await harnessTests.retrieve(TEST_CONFIGS.harnessTests, { WPT_DIR }).catch(err => { throw err })
-    await wptServer(harnessTestUrls, TEST_CONFIGS, { WPT_SERVER_PORT, WPT_DIR })
+    await wptServer(harnessTestUrls, TEST_CONFIGS, { WPT_SERVER_ADDRESS, WPT_SERVER_PORT, WPT_DIR })
 })()
