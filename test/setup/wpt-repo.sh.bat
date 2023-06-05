7a29eedfc158fd48ff2420aef4b8e359437d2cbd
Eugene Girard <girard@chromium.org>
2023-06-05 15:30:16 -0400

    Support win32 builds

--- test/setup/wpt-repo.sh.bat
+++ test/setup/wpt-repo.sh.bat
@@ -1,17 +1,4 @@
-rem #!/bin/bash
-
-rem PROJ_DIR="$(cd "$(dirname "$1")" && pwd)"
-
-rem WPT_DIR="$PROJ_DIR/test/wpt"
-
-rem # if directory does not exist, clone fresh-stuff from WPT master
-rem if [ ! -d "$WPT_DIR" ]; then
-rem   echo "WPT folder not found, cloning from remote..."
-rem   git clone https://github.com/web-platform-tests/wpt $WPT_DIR
-rem else
-rem   # ask git to attempt to rebase our changes on top of remote
-rem   git -C "$WPT_DIR" pull --ff-only
-rem fi
+rem Batch file version of wpt-repo.sh
 
 set PROJ_DIR=%CD%
 set WPT_DIR=%PROJ_DIR%\test\wpt
