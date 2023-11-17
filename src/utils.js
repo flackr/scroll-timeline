e7799de15d44b9d35cbfafc3060c2d1b2a39bcc0
Johannes Odland <johannes.odland@nrk.no>
2023-11-17 15:34:41 +0100

    Support CSSMathValue as rangeStart and rangeEnd if it can be simplified to a CSSUnitValue with px as unit. (#171)

--- src/utils.js
+++ src/utils.js
@@ -12,3 +12,9 @@ export function parseLength(obj, acceptStr) {
   }
   return null;
 }
+
+const canonicalUnits = new Set(["px", "deg", "s", "hz", "dppx", "number", "fr"]);
+
+export function isCanonical(unit) {
+  return canonicalUnits.has(unit.toLowerCase());
+}
