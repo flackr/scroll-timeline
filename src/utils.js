37956a91bdcc7b5f1efe2f0c23dbac9a8a94b70f
Bramus <bramus@bram.us>
2023-12-25 23:34:48 +0100

    Remove unused intersection-based-offset + used util function

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,3 @@
-export function parseLength(obj, acceptStr) {
-  if (obj instanceof CSSUnitValue || obj instanceof CSSMathSum)
-    return obj;
-  if (!acceptStr)
-    return null;
-  let matches = obj.trim().match(/^(-?[0-9]*\.?[0-9]*)(px|%)$/);
-  if (matches) {
-    let value = matches[1];
-    // The unit for % is percent.
-    let unit = matches[2] == '%' ? 'percent' : matches[2];
-    return new CSSUnitValue(value, unit);
-  }
-  return null;
-}
-
 const canonicalUnits = new Set(["px", "deg", "s", "hz", "dppx", "number", "fr"]);
 
 export function isCanonical(unit) {
