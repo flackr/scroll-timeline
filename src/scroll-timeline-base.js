73e022363736275bd49869db2e7c12e636940cd2
Johannes Odland <johannes.odland@gmail.com>
2024-01-25 17:15:57 +0100

    Account for insets when comparing size of subject to size of scrollport/view progress range

--- src/scroll-timeline-base.js
+++ src/scroll-timeline-base.js
@@ -669,7 +669,8 @@ export function calculateRange(phase, sourceMeasurements, subjectMeasurements, a
 
   let startOffset = undefined;
   let endOffset = undefined;
-  const targetIsTallerThanContainer = viewSize > sizes.containerSize ? true : false;
+  const adjustedScrollportSize = sizes.containerSize - inset.start - inset.end;
+  const subjectIsLargerThanScrollport = viewSize > adjustedScrollportSize;
 
   switch(phase) {
     case 'cover':
@@ -694,11 +695,11 @@ export function calculateRange(phase, sourceMeasurements, subjectMeasurements, a
 
     case 'entry-crossing':
       startOffset = coverStartOffset;
-      endOffset = targetIsTallerThanContainer ? containEndOffset : containStartOffset;
+      endOffset = subjectIsLargerThanScrollport ? containEndOffset : containStartOffset;
       break;
 
     case 'exit-crossing':
-      startOffset = targetIsTallerThanContainer ? containStartOffset : containEndOffset;
+      startOffset = subjectIsLargerThanScrollport ? containStartOffset : containEndOffset;
       endOffset = coverEndOffset;
       break;
   }
