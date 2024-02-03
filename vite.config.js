64d7c9dc222fa551b92c8c31c745ba1ecd6b7c19
Robert Flack <flackr@gmail.com>
2024-02-03 13:19:51 -0500

    Switch to using vite to build.

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    sourcemap: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'ScrollTimeline',
+      // the proper extensions will be added
+      fileName: (format, entryAlias) => `scroll-timeline${format=='iife'?'':'-' + format}.js`,
+      formats: ['iife']
+    },
+    rollupOptions: {
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+        },
+      },
+    },
+  },
+})
